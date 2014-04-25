691ca603f04a40272e5dbbbbd6fc326d9f33c45b
drew1d <andrelo1d@hotmail.com>
2014-04-24 21:19:37 -0500

    script creation

---
+++ cachematrix.R
@@ -0,0 +1,14 @@
+#___________________________________________________________________________________________________ 
+# Programming Assignment 2
+
+#___________________________________________________________________________________________________
+# makeCacheMatrix
+# makeCacheMatrix is a function that creates a special matrix object that can cache its inverse.
+
+
+
+#___________________________________________________________________________________________________
+# cacheSolve
+# cacheSolve returns the inverse of the special matrix created by makeCacheMatrix.
+# If the cached inverse matrix is available, cacheSolve retrieves it, if not, it computes, caches, 
+# and returns it.
